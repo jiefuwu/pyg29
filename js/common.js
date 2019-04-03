ebdcf1ac1e00824f1d2252d60370f2c10c1c0357
wlx <2649915671@qq.com>
2019-04-03 21:25:20 +0800

    初步完成首页，待完善

---
+++ js/common.js
@@ -0,0 +1,17 @@
+$(function () {
+  // 定义一个不可变变量。用来储存拼接路径的前部分
+  const baseURL = 'http://157.122.54.189:9094/api/public/v1/'
+  // 添加zepto拦截器，每个ajax请求都要经过拦截器
+  // beforeSend是请求发送时处理事件
+  $.ajaxSettings.beforeSend=function (xhr,obj) {
+    // xhr是异步对象
+    console.log(xhr);
+    // obj是请求对象.内包含发送到服务器的请求的数据
+    console.log(obj);
+    // 把请求对象中的路径进行拼接后重新赋值
+    obj.url=baseURL+obj.url
+  }
+  $.ajaxSettings.complete=function () {
+    
+  }
+})
--- js/pyg_index.js
+++ js/pyg_index.js
@@ -1,23 +1,34 @@
 $(function () {
-  $.ajax({
-    type: 'get',
-    url: 'http://157.122.54.189:9094/api/public/v1/home/swiperdata',
-    dataType: 'json',
-    success: function (result) {
-      console.log(result);
-      if (result.meta.status == 200) {
-        var html = template('pyg_bannerTemp', result)
-        $('.pyg_tu').html(html);
-        var indexhtml=template('pyg_bannerIndex',result)
-        $('.pyg_dian').html(indexhtml)
-        mui('.mui-slider').slider({
-          interval: 2000 //自动轮播周期，若为0则不自动播放，默认为0；
-        });
-      }
-    }
-  })
- 
-
-
+pyg_lbt()
+pyg_cpk()
 
 })
+function pyg_lbt() {
+   $.ajax({
+     type: 'get',
+     url: 'home/swiperdata',
+     dataType: 'json',
+     success: function (result) {
+       if (result.meta.status == 200) {
+         var html = template('pyg_bannerTemp', result)
+         $('.pyg_tu').html(html);
+         var indexhtml = template('pyg_bannerIndex', result)
+         $('.pyg_dian').html(indexhtml)
+         mui('.mui-slider').slider({
+           interval: 2000
+         });
+       }
+     }
+   })
+}
+function pyg_cpk() {
+  $.ajax({
+    type:'get',
+    url:'home/goodslist',
+    dataType:"json",
+    success:function (result) {
+      var html = template('pyg_cpks', result)
+      $('.pyg_cp').html(html)
+    }
+  })
+}
