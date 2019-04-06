b61abc004df126b4b6682c5535dce949f1aae112
wlx <2649915671@qq.com>
2019-04-06 14:58:14 +0800

    第3次提交，基本完成（差正在加载）

--- js/mtehod.js
+++ js/mtehod.js
@@ -1,29 +1,27 @@
 $(function () {
-  var cateData;
-  var index
-  mui('body').on('tap','a', function () {
-    window.top.location.href = this.href;
-  });
+  var cateData
+  mui('body').on('tap', 'a', function () {
+    window.top.location.href = this.href
+  })
   render()
   // 进行页面加载渲染
-  function render() {
-    cateData=JSON.parse(localStorage.getItem('pyg_cate'))
-    if (cateData && Date.now()-cateData.time<60*60*1000) {
+  function render () {
+    cateData = JSON.parse(localStorage.getItem('pyg_cate'))
+    if (cateData && Date.now() - cateData.time < 60 * 60 * 1000) {
       leftList()
       rightList(0)
-    }
-    else{
+    }else {
       getcate()
     }
   }
   // 发送ajax请求获取数据
-  function getcate() {
+  function getcate () {
     $.get('categories', function (result) {
       // 判断请求状态码是否正确
       if (result.meta.status == 200) {
         // 给自定义的全局变量赋值
         // 获取数据的data内容
-        当前时间
+        // 当前时间
         cateData = {
           'data': result.data,
           time: Date.now()
@@ -31,16 +29,15 @@ $(function () {
         // 进行本地储存数据
         localStorage.setItem('pyg_cate', JSON.stringify(cateData))
         // 进行页面的内容模块渲染
-        leftList()
-        rightList(0)
+        // leftList()
+        // rightList(0)
+        render()
       }
     }, 'json')
   }
-  
-
 
   // 渲染左边分类
-  function leftList() {
+  function leftList () {
     var html = template('fication', cateData)
     $('.leftlist').html(html)
     // 元素滚动初始化
@@ -49,7 +46,7 @@ $(function () {
   }
 
   // 左边单击事件
-  function tapceta(myScroll) {
+  function tapceta (myScroll) {
     // 移动端单击事件。给a进行事件委托给类名leftlist
     $('.left .leftlist').on('tap', 'a', function () {
       // 给当前元素添加类名active，其他兄弟元素移除
@@ -61,18 +58,17 @@ $(function () {
   }
 
   // 渲染右边分类
-  function rightList(index) {
+  function rightList (index) {
     // 进行动态渲染页面
-    var html=template('rightdata',cateData.data[index])
-    $('.rightda').html(html);
+    var html = template('rightdata', cateData.data[index])
+    $('.rightda').html(html)
     // 判断图片是否加载完毕。加载完毕后再进行滚动事件初始化
     var imgs = $('.rightda img').length
-    $('.rightda img').on('load',function () {
+    $('.rightda img').on('load', function () {
       imgs--
-      if (imgs==0) {
+      if (imgs == 0) {
         var myScroll = new IScroll('.right')
       }
     })
   }
 })
-
